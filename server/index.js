ee0be1be4295b40b7fe5e9f1be42d91f583ada20
Amandeep Singh <programmeramandeep@gmail.com>
2023-10-21 23:45:53 +0530

    Connected to th DB and setup env file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to the database'))
+  .catch((error) => console.log(error));
 
 app.get('/', (req, res) => {
   res.send('Hello world');
