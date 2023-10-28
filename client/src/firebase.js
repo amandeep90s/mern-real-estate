4cf31b62d61b22d99f7e4f160cddb91f55aebe4f
Amandeep Singh <programmeramandeep@gmail.com>
2023-10-28 23:01:39 +0530

    Correct the env variable name

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KET,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
