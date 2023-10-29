c90e431b6a3a67bb598b976710c9e9ba653472ed
Amandeep Singh <programmeramandeep@gmail.com>
2023-10-29 12:02:33 +0530

    Added redirection to home after login with google

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { app } from '../firebase';
-import { signInSuccess } from '../redux/user/userSlice.js';
+import { signInFailure, signInSuccess } from '../redux/user/userSlice.js';
 
 const OAuth = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleGoogleClick = async () => {
     try {
@@ -27,8 +29,10 @@ const OAuth = () => {
       });
 
       dispatch(signInSuccess(response));
+      navigate('/');
     } catch (error) {
       console.log('Could not sign in with google', error);
+      dispatch(signInFailure(error?.response?.data?.message || error?.message));
     }
   };
 
