d920d8a5768047eb16d1bb189b9fb329a22e3062
Amandeep Singh <programmeramandeep@gmail.com>
2023-12-17 18:18:14 +0530

    Completed home page ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,168 @@
-import React from 'react';
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import SwiperCore from "swiper";
+import "swiper/css/bundle";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ListingItem from "../components/ListingItem";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+
+  SwiperCore.use([Navigation]);
+
+  const fetchOfferListings = useCallback(async () => {
+    try {
+      const result = await axios.get(`/api/listing?offer=true&limit=4`);
+
+      if (result.data.status) {
+        setOfferListings(result.data.listings);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const fetchRentListings = useCallback(async () => {
+    try {
+      const result = await axios.get(`/api/listing?type=rent&limit=4`);
+
+      if (result.data.status) {
+        setRentListings(result.data.listings);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const fetchSaleListings = useCallback(async () => {
+    try {
+      const result = await axios.get(`/api/listing?type=sale&limit=4`);
+
+      if (result.data.status) {
+        setSaleListings(result.data.listings);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchOfferListings();
+    fetchRentListings();
+    fetchSaleListings();
+  }, [fetchOfferListings, fetchRentListings, fetchSaleListings]);
+
+  return (
+    <div>
+      {/* top */}
+      <div className="flex flex-col max-w-6xl gap-6 px-3 mx-auto p-28">
+        <h1 className="text-3xl font-bold text-slate-700 lg:text-6xl">
+          Find your next <span className="text-slate-500">perfect</span>
+          <br />
+          place with ease
+        </h1>
+        <div className="text-xs text-gray-400 sm:text-sm">
+          Sahand Estate is the best place to find your next perfect place to
+          live.
+          <br />
+          We have a wide range of properties for you to choose from.
+        </div>
+        <Link
+          to={"/search"}
+          className="text-xs font-bold text-blue-800 sm:text-sm hover:underline"
+        >
+          Let&apos;s get started...
+        </Link>
+      </div>
+
+      {/* swiper */}
+      <Swiper navigation>
+        {offerListings &&
+          offerListings.length > 0 &&
+          offerListings.map((listing) => (
+            <SwiperSlide key={listing._id}>
+              <div
+                style={{
+                  background: `url(${listing.imageUrls[0]}) center no-repeat`,
+                  backgroundSize: "cover",
+                }}
+                className="h-[500px]"
+                key={listing._id}
+              ></div>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      {/* listing results for offer, sale and rent */}
+
+      <div className="flex flex-col max-w-6xl gap-8 p-3 mx-auto my-10">
+        {offerListings && offerListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent offers
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={"/search?offer=true"}
+              >
+                Show more offers
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {offerListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {rentListings && rentListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent places for rent
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={"/search?type=rent"}
+              >
+                Show more places for rent
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {rentListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+        {saleListings && saleListings.length > 0 && (
+          <div className="">
+            <div className="my-3">
+              <h2 className="text-2xl font-semibold text-slate-600">
+                Recent places for sale
+              </h2>
+              <Link
+                className="text-sm text-blue-800 hover:underline"
+                to={"/search?type=sale"}
+              >
+                Show more places for sale
+              </Link>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {saleListings.map((listing) => (
+                <ListingItem listing={listing} key={listing._id} />
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,19 +1,19 @@
-import express from 'express';
+import express from "express";
 import {
   createListing,
   deleteListing,
   getAllListings,
   getListing,
   updateListing,
-} from '../controllers/listing.controller.js';
-import { verifyToken } from '../middlewares/verifyUser.js';
+} from "../controllers/listing.controller.js";
+import { verifyToken } from "../middlewares/verifyUser.js";
 
 const router = express.Router();
 
-router.get('/', verifyToken, getAllListings);
-router.post('/', verifyToken, createListing);
-router.get('/:id', getListing);
-router.put('/:id', verifyToken, updateListing);
-router.delete('/:id', verifyToken, deleteListing);
+router.get("/", getAllListings);
+router.post("/", verifyToken, createListing);
+router.get("/:id", getListing);
+router.put("/:id", verifyToken, updateListing);
+router.delete("/:id", verifyToken, deleteListing);
 
 export default router;
