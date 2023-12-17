3e9af7152060a8dd3fabe5ff6cae4e2cedc4eb0e
Amandeep Singh <programmeramandeep@gmail.com>
2023-12-17 17:23:51 +0530

    Completed listing item and about us page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className='max-w-6xl px-4 py-20 mx-auto'>
+      <h1 className='mb-4 text-3xl font-bold text-slate-800'>
+        About Real Estate
+      </h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius id
+        provident eveniet minus, quia explicabo nemo consequatur cumque
+        distinctio enim dolore harum corporis porro quo exercitationem odit
+        consectetur! Voluptatibus nobis earum consequatur ducimus. Blanditiis
+        vel corrupti quo veritatis alias atque corporis, saepe est nostrum
+        officia in, vitae magni ratione rerum.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi, eaque
+        exercitationem. Quos distinctio repudiandae vel quo eligendi,
+        necessitatibus iste quidem perferendis expedita quibusdam repellat dicta
+        placeat numquam reiciendis illum, autem dolor eum id fugit ea obcaecati.
+        Ea, voluptatibus quae deleniti dolorum obcaecati quaerat reprehenderit
+        cumque atque deserunt iusto eveniet culpa.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit hic
+        omnis excepturi quasi adipisci earum iusto ad, vel nihil ratione.
+        Tenetur saepe labore recusandae reiciendis voluptatum, eligendi rerum.
+        Doloribus, architecto atque magnam ipsum perspiciatis accusantium, quasi
+        ex necessitatibus officiis reprehenderit voluptatem ducimus minus facere
+        adipisci, maxime sequi quibusdam cupiditate excepturi vero. Quisquam
+        aliquid unde dicta nostrum fugiat, nihil velit dignissimos officiis.
+        Deserunt unde nihil exercitationem dolores odio eum blanditiis
+        consectetur.
+      </p>
+    </div>
+  );
 };
 
 export default About;
